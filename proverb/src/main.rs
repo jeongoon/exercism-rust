6dfeb8f2be0321d7e45e3cfdb4a23c60cc42c06e
Myoungjin JEON <jeongoon@gmail.com>
2022-05-19 22:12:43 +1000

    add more version of "proverb"

--- proverb/src/main.rs
+++ proverb/src/main.rs
@@ -1,3 +1,17 @@
+pub fn build_proverb_1(list: &[&str]) -> String {
+    match list.len() {
+        0 => "".to_owned(),
+        _ => {
+            let mut story = list
+                .windows(2)
+                .map(|x| format!("For want of a {} the {} was lost.", x[0], x[1],))
+                .collect::<Vec<String>>();
+            story.push(format!("And all for the want of a {}.", list[0]));
+            story.join("\n")
+        }
+    }
+}
+
 pub fn build_proverb(list: &[&str]) -> String {
     match list.first() {
         None => String::from(""),
